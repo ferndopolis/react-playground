e9629f773a611612c86976c33d871e7341d48823
Fernando Marcheselli <fmarcheselli@gmail.com>
2015-12-17 12:23:43 -0800

    separate Gallery component into own file

--- playground/gallery/app.js
+++ playground/gallery/app.js
@@ -1,8 +1,7 @@
 require("./app.less")
-require("font-awesome-webpack")
 
-import classnames from 'classnames'
 import React, {Component} from 'react'
+import Gallery from './Gallery'
 
 class App extends Component {
     constructor () {
@@ -29,98 +28,5 @@ class App extends Component {
         )
     }
 }
-
-class Gallery extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            currentImg: 0
-        }
-
-    }
-
-    galleryLength() {
-        return this.props.images.length
-
-    }
-
-    hasPrevItem() {
-        return this.state.currentImg > 0
-    }
-
-    handleLeftClick () {
-        if ( this.hasPrevItem() ) {
-            this.setState({
-                currentImg: this.state.currentImg - 1
-            })
-        } else {
-            if ( this.props.loop ) {
-                this.setState({ currentImg: this.galleryLength() - 1 })
-            }
-        }
-    }
-
-    hasNextItem() {
-        return this.state.currentImg < this.galleryLength() - 1
-    }
-
-    handleRightClick () {
-        if ( this.hasNextItem() ) {
-            this.setState({ currentImg: this.state.currentImg + 1 })
-        } else {
-            // has hit the upper limit
-            if ( this.props.loop ) {
-                // go back to first image
-                this.setState({ currentImg: 0})
-            }
-        }
-    }
-
-    isActive(i) {
-        if ( i == this.state.currentImg ) {
-            return true
-        } else {
-            return false
-        }
-    }
-
-    render() {
-        var self = this
-        var images = []
-
-
-        this.props.images.forEach( (image, i) => {
-            var classes = classnames({
-                'image-container': true,
-                'active': this.isActive(i),
-                'hide': !this.isActive(i)
-            })
-
-            images.push(
-                <li key={i} className={classes}>
-                    <h3 className="image-title">{image.title}</h3>
-                    <img className="centered" src={image.src} />
-                </li>
-            )
-        })
-
-        return (
-            <div>
-                <i onClick={self.handleLeftClick.bind(self)}
-                   className="fa fa-chevron-left left"></i>
-                <ul className="images-container">{ images }</ul>
-                <i onClick={self.handleRightClick.bind(self)}
-                   className="fa fa-chevron-right right"></i>
-            </div>
-        )
-    }
-}
-
-Gallery.propTypes = {
-    loop: React.PropTypes.bool,
-    images: React.PropTypes.array.isRequired
-}
-
-Gallery.defaultProps = { loop: false }
 
 React.render(<App/>, document.getElementById('example'));
