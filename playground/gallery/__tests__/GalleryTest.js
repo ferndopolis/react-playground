e4f17ff7b090d480b6e20a3a5d895be8263e7735
Fernando Marcheselli <fmarcheselli@gmail.com>
2015-12-18 14:18:52 -0800

    add tests for hasPrevItem(), hasNextItem(), galleryLenght()

--- playground/gallery/__tests__/GalleryTest.js
+++ playground/gallery/__tests__/GalleryTest.js
@@ -15,14 +15,40 @@ describe("Gallery", () => {
         {src: "https://unsplash.it/120/250", title: 'Burgers'}
     ];
 
-    beforeEach(function () {
+    beforeEach(() => {
         gallery = TestUtils.renderIntoDocument(
             <Gallery images={ images }/>
         );
     });
 
-    it("initial state", function () {
+    it("initial state", () => {
         expect(gallery.props.loop).toBe(false);
         expect(gallery.state.currentImg).toBe(0);
+    });
+
+    describe("hasPrevItem()", () => {
+        it("when first item return false", () => {
+            expect(gallery.hasPrevItem()).toBe(false);
+        });
+        it("when last item return true", () => {
+            gallery.state.currentImg = 2
+            expect(gallery.hasPrevItem()).toBe(true);
+        });
+    });
+
+    describe("hasNextItem()", () => {
+        it("when first item return true", () => {
+            expect(gallery.hasNextItem()).toBe(true);
+        });
+        it("when last item return false", () => {
+            gallery.state.currentImg = 2
+            expect(gallery.hasNextItem()).toBe(false);
+        });
+    });
+
+    describe("galleryLength()", () => {
+        it("should return gallery item count", () => {
+            expect(gallery.galleryLength()).toBe(3);
+        });
     });
 });
