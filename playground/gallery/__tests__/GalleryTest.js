927a7fa36dfa0cd285e36961ccfea575b879e4e7
Fernando Marcheselli <fmarcheselli@gmail.com>
2015-12-23 13:52:15 -0800

    spec for Gallery.isActive(), isPrev(), isNext()

--- playground/gallery/__tests__/GalleryTest.js
+++ playground/gallery/__tests__/GalleryTest.js
@@ -7,7 +7,7 @@ import TestUtils from 'react-addons-test-utils';
 const Gallery = require('../Gallery');
 
 describe("Gallery", () => {
-    var gallery;
+    var gallery, galleryWithLoop;
 
     var images = [
         {src: "https://unsplash.it/800/600", title: 'Basil'},
@@ -18,6 +18,10 @@ describe("Gallery", () => {
     beforeEach(() => {
         gallery = TestUtils.renderIntoDocument(
             <Gallery images={ images }/>
+        );
+
+        galleryWithLoop = TestUtils.renderIntoDocument(
+            <Gallery loop={true} images={ images }/>
         );
     });
 
@@ -49,6 +53,75 @@ describe("Gallery", () => {
     describe("galleryLength()", () => {
         it("should return gallery item count", () => {
             expect(gallery.galleryLength()).toBe(3);
+        });
+    });
+
+    describe("isAcitve()", () => {
+        it("should return true when currentImg", () => {
+            gallery.state.currentImg = 2
+            expect(gallery.isActive(2)).toBe(true);
+        });
+        it("should return false when not currentImg", () => {
+            gallery.state.currentImg = 1
+            expect(gallery.isActive(2)).toBe(false);
+        });
+    });
+
+    describe("isNext()", () => {
+        it("should return true when image follows directly after currentImg", () => {
+            gallery.state.currentImg = 1
+            expect(gallery.isNext(2)).toBe(true);
+        });
+        it("should return false when not follows directly after currentImg", () => {
+            gallery.state.currentImg = 1
+            expect(gallery.isNext(0)).toBe(false);
+        });
+        it("should return false on currentImg", () => {
+            gallery.state.currentImg = 1
+            expect(gallery.isNext(1)).toBe(false);
+        });
+        describe("when loop is true", () => {
+            it("should return true only on first image when current image is last", () => {
+                galleryWithLoop.state.currentImg = 2
+                expect(galleryWithLoop.isNext(0)).toBe(true);
+            });
+            it("should return false on other images when current image is last", () => {
+                gallery.state.currentImg = 2
+                expect(gallery.isNext(1)).toBe(false);
+            });
+            it("should return false on current image ", () => {
+                gallery.state.currentImg = 2
+                expect(gallery.isNext(2)).toBe(false);
+            });
+        });
+    });
+
+    describe("isPrev()", () => {
+        it("should return true when image follows directly before currentImg", () => {
+            gallery.state.currentImg = 1
+            expect(gallery.isPrev(0)).toBe(true);
+        });
+        it("should return false when does not follows directly before currentImg", () => {
+            gallery.state.currentImg = 1
+            expect(gallery.isPrev(2)).toBe(false);
+        });
+        it("should return false on currentImg", () => {
+            gallery.state.currentImg = 1
+            expect(gallery.isPrev(1)).toBe(false);
+        });
+        describe("when loop is true", () => {
+            it("should return true only on last image when current image is first", () => {
+                galleryWithLoop.state.currentImg = 0
+                expect(galleryWithLoop.isPrev(2)).toBe(true);
+            });
+            it("should return false on other images when current image is first", () => {
+                gallery.state.currentImg = 0
+                expect(gallery.isPrev(1)).toBe(false);
+            });
+            it("should return false on current image ", () => {
+                gallery.state.currentImg = 2
+                expect(gallery.isPrev(2)).toBe(false);
+            });
         });
     });
 
@@ -100,7 +173,6 @@ describe("Gallery", () => {
 
         describe("when loop is true", () => {
             it("on first item should loop to last", () => {
-                var galleryWithLoop = TestUtils.renderIntoDocument(<Gallery loop={true} images={ images }/>);
                 TestUtils.Simulate.click(
                     TestUtils.findRenderedDOMComponentWithClass(galleryWithLoop, 'left')
                 );
